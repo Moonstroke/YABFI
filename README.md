2c0391ebb1725d538cc248b23b9f9568f6f8fc3c
joH1 <jiema@live.fr>
2021-08-06 13:21:59 +0200

    Add source and write README

--- README.md
+++ README.md
@@ -2,3 +2,32 @@
 ## Yet Another BrainFuck Interpreter
 
 C89-compatible [Brainfuck](https://en.wikipedia.org/wiki/Brainfuck) interpreter
+
+
+### Presentation
+
+The current implementation has a low static memory footprint (it uses 64 KiB of
+memory). The counterpart is that the program's data is capped at 32 KiB (32768
+unsigned octets exactly), and the loop depth is limited to 512 levels of
+nesting.
+
+Proper bound-checking is also implemented to provide a safe program, and various
+result codes are used by the executable interpreter to identify the source of
+failure when the executions ends with error.
+
+
+### Usage
+
+The executable accepts a single argument, a character string containing the
+Brainfuck code to execute (using command-line options to read from a file is a
+planned evolution).
+
+It reads from `stdin`, and outputs to `stdout`.
+
+
+### Compilation
+
+Just run `make` to compile, or use your own compiler.
+
+Other Make rules provided are `debug`, to build a debuggable executable, and
+`clean` to remove the compiled files (objects and executable).
---
+++ yabfi.c
@@ -0,0 +1,93 @@
+#include <stdint.h> /* for uint8_t */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#define TAPE_SIZE 32768
+#define MAX_LOOP_DEPTH 512
+
+
+enum bf_error {
+	BF_SUCCESS,
+	BF_ERROR_TAPE_OVERFLOW,
+	BF_ERROR_TAPE_UNDERFLOW,
+	BF_ERROR_IO,
+	BF_ERROR_LOOP_OVERFLOW,
+	BF_ERROR_LOOP_UNDERFLOW,
+};
+
+
+enum bf_error parse(const char *program, const char **loop_opens, const char **loop_closes) {
+	size_t depth = 0;
+	for (; *program; ++program) {
+		if (*program == '[') {
+			loop_opens[depth++] = program;
+			if (depth == MAX_LOOP_DEPTH) return BF_ERROR_LOOP_OVERFLOW;
+		} else if (*program == ']') {
+			if (depth == 0) return BF_ERROR_LOOP_UNDERFLOW;
+			loop_closes[--depth] = program;
+		}
+	}
+	return BF_SUCCESS;
+}
+
+enum bf_error run(const char *program, uint8_t *tape, const char *const *loop_opens, const char *const *loop_closes) {
+	size_t tape_pointer = 0;
+	size_t loop_depth = 0;
+	for (; *program; ++program) {
+		switch (*program) {
+			case '>':
+				if (++tape_pointer == TAPE_SIZE) return BF_ERROR_TAPE_OVERFLOW;
+				break;
+			case '<':
+				if (tape_pointer-- == 0) return BF_ERROR_TAPE_UNDERFLOW;
+				break;
+			case '+':
+				tape[tape_pointer]++;
+				break;
+			case '-':
+				tape[tape_pointer]--;
+				break;
+			case ',':
+				{
+					int input = getchar();
+					tape[tape_pointer] = (uint8_t) (input == EOF ? 0 : input);
+				}
+				break;
+			case '.':
+				if (putchar(tape[tape_pointer]) == EOF) return BF_ERROR_IO;
+				break;
+			case '[':
+				if (tape[tape_pointer] == 0) {
+					program = loop_closes[loop_depth];
+				} else {
+					++loop_depth;
+				}
+				break;
+			case ']':
+				if (tape[tape_pointer] == 0) {
+					--loop_depth;
+				} else {
+					program = loop_opens[loop_depth - 1];
+				}
+				break;
+		}
+	}
+	return BF_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		fprintf(stderr, "USAGE: %s BRAINFUCK_CODE\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	uint8_t tape[TAPE_SIZE] = {0};
+	const char *loop_opens[MAX_LOOP_DEPTH] = {0};
+	const char *loop_closes[MAX_LOOP_DEPTH] = {0};
+	int rc = parse(argv[1], loop_opens, loop_closes);
+	if (rc == BF_SUCCESS) {
+		rc = run(argv[1], tape, loop_opens, loop_closes);
+	}
+	return rc;
+}
